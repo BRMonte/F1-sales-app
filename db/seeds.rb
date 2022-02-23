8b3b3806807868bd9bf999583d3b169ccf55462f
Bruno Montebello <brunormontebello@hotmail.com>
2022-02-23 06:07:56 -0300

    Add csv file + seed DB with csv data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db/lost-leads.csv'))
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
+csv.each do |row|
+  t = LeadRequest.new
+  p t.lead_id = row['id']
+  p t.lead_name = row['nome_completo']
+  t.save!
+end
